106639cf306be29d7df79875ffe42d01bb3c93e3
polyfloyd <floyd@polyfloyd.net>
2017-10-10 19:08:22 +0200

    Add an integration test for generic pipes

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,8 +5,18 @@ extern crate assert_cli;
 fn calling_without_args() {
     assert_cli::Assert::main_binary()
         .fails()
-        .and()
         .stderr().contains("USAGE")
         .stdout().is("")
+        .unwrap();
+}
+
+#[test]
+fn calling_generic_pipe() {
+    let data = "\x00\x01\x02\x03\x04\x05\x06\x07\x08";
+    assert_cli::Assert::main_binary()
+        .with_args(&["--geometry", "3", "--driver", "none", "generic"])
+        .stdin(data)
+        //.stderr().is("") TODO: https://github.com/killercup/assert_cli/issues/45
+        .stdout().is(data)
         .unwrap();
 }
