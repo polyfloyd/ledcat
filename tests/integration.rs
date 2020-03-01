1c37383da12174342802b01dbb398fb8bd28f4ac
polyfloyd <floyd@polyfloyd.net>
2020-03-01 15:09:43 +0100

    Update integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,8 +1,4 @@
-#![cfg(test)]
-extern crate assert_cli;
-
 #[test]
-#[cfg(not(feature = "ci"))]
 fn calling_without_args() {
     assert_cli::Assert::main_binary()
         .fails()
@@ -14,13 +10,13 @@ fn calling_without_args() {
 }
 
 #[test]
-#[cfg(not(feature = "ci"))]
 fn calling_generic_pipe() {
     let data = "\x00\x01\x02\x03\x04\x05\x06\x07\x08";
     assert_cli::Assert::main_binary()
         .with_args(&["--geometry", "3", "--driver", "none", "generic"])
         .stdin(data)
-        //.stderr().is("") TODO: https://github.com/killercup/assert_cli/issues/45
+        .stderr()
+        .is("")
         .stdout()
         .is(data)
         .unwrap();
